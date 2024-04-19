08ad96223804bc4225a22b7733a39f39d2442f7a
Ethan <ethan.c.chen@gmail.com>
2024-04-19 01:14:46 -0700

    update code to do forecasting in `rlhf_dataloader.py`

--- train/rlhf_dataloader.py
+++ train/rlhf_dataloader.py
@@ -6,32 +6,28 @@ import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
-class StoryRLHF(Dataset):
-    def __init__(self, data_path: str, actions_path: str):
+class ForecastingRLHF(Dataset):
+    def __init__(self, data_path: str):
         self.data_path = data_path
         self.df = pd.read_csv(data_path)
         self.instruction_template = "Below is an instruction that describes a task. " + \
                           "Write a response that appropriately completes the request.\n\n" + \
                           "### Instruction:\n{instruction}\n\n### Response:"
-        self.prompt_template = """Here is a story prompt: {prompt}\n\nHere is the first paragraph of the story:{init_paragpraph}.\n\n Here is a set of actions: {actions}.\n\nBased on the initial paragraph, choose the best action for the next paragraph. Only output the action you chose without any quotation marks."""
-        self.actions_path = actions_path
-        with open(actions_path, 'r') as file:
-            self.actions = json.load(file)
+        self.prompt_template = """Here is a forecasting question: {question}\n\nHere are the resolution criteria of the question: {resolution_criteria}.\n\nBased on the forecasting question and resolution criteria, generate {quality_type} quality step-by-step reasoning to create a resolution. Output only steps without an introduction or a conclusion."""
 
     def __len__(self):
         return len(self.df)
     
-    def format_prompt(self, story_idea, init_paragraph):
+    def format_prompt(self, question, resolution_criteria):
         return self.instruction_template.format(
                     instruction=self.prompt_template.format(
-                        prompt=story_idea, 
-                        init_paragpraph=init_paragraph,                     
-                        actions=self.actions))
+                        question=question, 
+                        resolution_criteria=resolution_criteria))
     
     def get_dataset(self):
         chosen = self.df['chosen'].tolist()
         rejected = self.df['rejected'].tolist()
-        prompts = [self.format_prompt(self.df['prompt'][idx], self.df['initial_paragraph'][idx]) for idx in range(len(self.df))]
+        prompts = [self.format_prompt(self.df['question'][idx], self.df['resolution_criteria'][idx]) for idx in range(len(self.df))]
         dataset = [{"prompt": prompts[i], "chosen": chosen[i], "rejected": rejected[i]} for i in range(len(prompts))]
         return dataset
 
