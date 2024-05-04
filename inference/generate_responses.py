2379936690b85008e4e259d8e77efc30871c4e5c
Ethan <ethan.c.chen@gmail.com>
2024-05-04 13:11:42 -0700

    add `Inferencer` class to get all LlaMA responses; remove some logger

--- inference/generate_responses.py
+++ inference/generate_responses.py
@@ -1,10 +1,18 @@
 import logging
 import os
+import sys
 from pathlib import Path
 
 import pandas as pd
 import torch
+from openai import OpenAI
+from pandas import Index
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextGenerationPipeline
+
+parent_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(parent_dir))
+
+from utils.helpers import get_openai_api_key  # noqa: E402
 
 # Setup logging
 logging.basicConfig(
@@ -17,28 +25,83 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-MAX_MAX_NEW_TOKENS = 2048
-DEFAULT_MAX_NEW_TOKENS = 1024
+MAX_MAX_NEW_TOKENS = 16000
+# DEFAULT_MAX_NEW_TOKENS = 1024
 MAX_INPUT_TOKEN_LENGTH = int(os.getenv("MAX_INPUT_TOKEN_LENGTH", "4096"))
 MODEL_ID = "meta-llama/Meta-Llama-3-8B"
 CUSTOM_CACHE_DIR = Path(
     "/", "global", "scratch", "users", "ethancchen", ".cache", "huggingface", "models--meta-llama--Meta-Llama-3-8B"
 )
+GENERAL_PROMPT = "general_prompt"
+LLAMA_RESPONSE = "llama_response"
+QUESTION = "question"
+RESOLUTION_CRITERIA = "resolution_criteria"
+
+INPUT_FILEPATH = Path(__file__).resolve().parents[1] / "data" / "sampled_100_test_hf_dataset.csv"
 OUTPUT_DIR = Path(__file__).resolve().parent / "output"
 
 
-def generate(queries: list[str], max_new_tokens: int = 1024) -> list[str]:
-    responses = []
-    text_generator = TextGenerationPipeline(model, tokenizer)
+class Inferencer:
+    def __init__(self, data_path: Path) -> None:
+        self.data_path = data_path
+        self.df = pd.read_csv(data_path)
+        self.general_prompt_template = """Here is a forecasting question: {question}\n\nHere are the resolution criteria of the question: {resolution_criteria}.\n\nBased on the forecasting question and resolution criteria, generate step-by-step reasoning to create a resolution. Output exactly 6 steps without an introduction or a conclusion."""  # noqa: E501
+        self.client = OpenAI(api_key=get_openai_api_key())
 
-    for query in queries:
-        logger.info(f"Processing query: {query}")
-        output = text_generator(query, max_length=MAX_INPUT_TOKEN_LENGTH + max_new_tokens)
-        response = output[0]["generated_text"]
-        responses.append(response)
-        logger.info(f"Generated response: {response}")
+    def __len__(self) -> int:
+        return len(self.df)
 
-    return responses
+    def _get_df_columns(self) -> Index:
+        return self.df.columns
+
+    def format_general_prompt(self, question: str, resolution_criteria: str) -> str:
+        assert len(question) > 0, f"Please provide a non-empty string for the {QUESTION}."
+        assert len(resolution_criteria) > 0, f"Please provide a non-empty string for the {RESOLUTION_CRITERIA}."
+        return self.general_prompt_template.format(
+            question=question,
+            resolution_criteria=resolution_criteria,
+        )
+
+    def populate_df_general_prompts(self) -> None:
+        assert GENERAL_PROMPT not in self._get_df_columns()
+        self.df[GENERAL_PROMPT] = self.df.apply(
+            lambda row: self.format_general_prompt(row[QUESTION], row[RESOLUTION_CRITERIA]), axis=1
+        )
+
+    def get_all_llama_responses(self):
+        assert LLAMA_RESPONSE not in self._get_df_columns()
+        text_generator = TextGenerationPipeline(model, tokenizer)
+        llama_responses = []
+
+        for _, row in self.df.iterrows():
+            query = row[GENERAL_PROMPT]
+            output = text_generator(query, max_length=MAX_INPUT_TOKEN_LENGTH + MAX_MAX_NEW_TOKENS)
+            response = output[0]["generated_text"]
+            llama_responses.append(response)
+
+        self.df.loc[:, LLAMA_RESPONSE] = llama_responses
+        output_filepath = OUTPUT_DIR / f"{MODEL_ID}_responses.csv"
+        self.df.to_csv(output_filepath)
+
+    def prompt_gpt35_once(self, prompt: str) -> str:
+        try:
+            response = self.client.chat.completions.create(
+                model="gpt-3.5-turbo-0125", messages=[{"role": "user", "content": prompt}]
+            )
+            return response.choices[0].message.content
+        except Exception as e:
+            print(f"Error processing prompt: {prompt}\nError: {e}")
+            return "Error in response"  # Placeholder or handle appropriately
+
+    # TODO: generate all GPT-3.5 responses separately so we don't generate them every time for each LlaMA.
+    def get_all_gpt35_responses(self) -> None:
+        # self.df.loc[] = [
+        #     self.prompt_gpt4_once(prompt) for prompt in self.df.loc[GENERAL_PROMPT]
+        # ]
+
+        # final_path = self.data_path.with_name("prepared_gpt4_responses_" + self.data_path.name)
+        # self.df.to_csv(final_path)
+        pass
 
 
 if __name__ == "__main__":
@@ -51,13 +114,6 @@ if __name__ == "__main__":
     else:
         logger.error("CUDA is not available. This script requires a GPU to run.")
         exit(1)
-    queries = [
-        "What is the impact of AI on society?",
-        "Tell me about the latest advancements in machine learning.",
-        "How does quantum computing affect data security?",
-    ]
-    responses = generate(queries)
-    for response in responses:
-        print(response)
-    output_filepath = OUTPUT_DIR / f"{MODEL_ID}_responses.csv"
-    pd.DataFrame({"query": queries, "response": responses}).to_csv(output_filepath, index=False)
+    inferencer = Inferencer(data_path=INPUT_FILEPATH)
+    inferencer.populate_df_general_prompts()
+    inferencer.get_all_llama_responses()
