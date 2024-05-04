d1781d479d9ae409a43270579a38ec416fad427c
Ethan <ethan.c.chen@gmail.com>
2024-05-04 16:34:57 -0700

    add `asyncio` to fetch all sampled size 100 test set w/ GPT-3.5; add that CSV

--- inference/generate_responses.py
+++ inference/generate_responses.py
@@ -1,3 +1,5 @@
+import argparse
+import asyncio
 import logging
 import os
 import sys
@@ -5,7 +7,7 @@ from pathlib import Path
 
 import pandas as pd
 import torch
-from openai import OpenAI
+from openai import AsyncOpenAI
 from pandas import Index
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextGenerationPipeline
 
@@ -14,7 +16,6 @@ sys.path.append(str(parent_dir))
 
 from utils.helpers import get_openai_api_key  # noqa: E402
 
-# Setup logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -34,19 +35,22 @@ CUSTOM_CACHE_DIR = Path(
 )
 GENERAL_PROMPT = "general_prompt"
 LLAMA_RESPONSE = "llama_response"
+GPT35_RESPONSE = "gpt35_response"
 QUESTION = "question"
 RESOLUTION_CRITERIA = "resolution_criteria"
 
 INPUT_FILEPATH = Path(__file__).resolve().parents[1] / "data" / "sampled_100_test_hf_dataset.csv"
 OUTPUT_DIR = Path(__file__).resolve().parent / "output"
+OUTPUT_FILENAME_SUFFIX = "_sampled_test_set_responses.csv"
 
 
 class Inferencer:
     def __init__(self, data_path: Path) -> None:
         self.data_path = data_path
         self.df = pd.read_csv(data_path)
+        assert len(self.df) == 100, "test set must have 100 rows of forecasting data"
         self.general_prompt_template = """Here is a forecasting question: {question}\n\nHere are the resolution criteria of the question: {resolution_criteria}.\n\nBased on the forecasting question and resolution criteria, generate step-by-step reasoning to create a resolution. Output exactly 6 steps without an introduction or a conclusion."""  # noqa: E501
-        self.client = OpenAI(api_key=get_openai_api_key())
+        self.client = AsyncOpenAI(api_key=get_openai_api_key())
 
     def __len__(self) -> int:
         return len(self.df)
@@ -68,24 +72,24 @@ class Inferencer:
             lambda row: self.format_general_prompt(row[QUESTION], row[RESOLUTION_CRITERIA]), axis=1
         )
 
-    def get_all_llama_responses(self):
+    def get_all_llama_responses(self, model, tokenizer):
         assert LLAMA_RESPONSE not in self._get_df_columns()
         text_generator = TextGenerationPipeline(model, tokenizer)
         llama_responses = []
 
         for _, row in self.df.iterrows():
-            query = row[GENERAL_PROMPT]
-            output = text_generator(query, max_length=MAX_INPUT_TOKEN_LENGTH + MAX_MAX_NEW_TOKENS)
+            prompt = row[GENERAL_PROMPT]
+            output = text_generator(prompt, max_length=MAX_INPUT_TOKEN_LENGTH + MAX_MAX_NEW_TOKENS)
             response = output[0]["generated_text"]
             llama_responses.append(response)
 
         self.df.loc[:, LLAMA_RESPONSE] = llama_responses
-        output_filepath = OUTPUT_DIR / f"{MODEL_ID}_responses.csv"
-        self.df.to_csv(output_filepath)
+        output_filepath = OUTPUT_DIR / f"{MODEL_ID}{OUTPUT_FILENAME_SUFFIX}"
+        self.df.to_csv(output_filepath, index=False)
 
-    def prompt_gpt35_once(self, prompt: str) -> str:
+    async def prompt_gpt35_once(self, prompt: str) -> str:
         try:
-            response = self.client.chat.completions.create(
+            response = await self.client.chat.completions.create(
                 model="gpt-3.5-turbo-0125", messages=[{"role": "user", "content": prompt}]
             )
             return response.choices[0].message.content
@@ -93,27 +97,56 @@ class Inferencer:
             print(f"Error processing prompt: {prompt}\nError: {e}")
             return "Error in response"  # Placeholder or handle appropriately
 
-    # TODO: generate all GPT-3.5 responses separately so we don't generate them every time for each LlaMA.
-    def get_all_gpt35_responses(self) -> None:
-        # self.df.loc[] = [
-        #     self.prompt_gpt4_once(prompt) for prompt in self.df.loc[GENERAL_PROMPT]
-        # ]
+    async def get_all_gpt35_responses(self) -> None:
+        """Call this only once. DO NOT call this along with every single LlaMA model generation."""
+        assert LLAMA_RESPONSE not in self._get_df_columns()
 
-        # final_path = self.data_path.with_name("prepared_gpt4_responses_" + self.data_path.name)
-        # self.df.to_csv(final_path)
-        pass
+        # Create a list of tasks for asynchronous execution
+        tasks = [self.prompt_gpt35_once(row[GENERAL_PROMPT]) for _, row in self.df.iterrows()]
+        gpt35_responses = await asyncio.gather(*tasks)
+
+        self.df[GPT35_RESPONSE] = gpt35_responses
+        output_filepath = OUTPUT_DIR / f"GPT-3.5{OUTPUT_FILENAME_SUFFIX}"
+        self.df.to_csv(output_filepath, index=False)
+
+
+async def main():
+    parser = argparse.ArgumentParser(description="Generate responses based on model type.")
+    parser.add_argument(
+        "--is_gpt",
+        choices=["True", "False"],
+        required=True,
+        help='A boolean. "True" for using GPT, "False" otherwise. Case-sensitive and required.',
+    )
+    args = parser.parse_args()
+    is_gpt = args.is_gpt
+    OUTPUT_DIR.mkdir(exist_ok=True)
+    inferencer = Inferencer(data_path=INPUT_FILEPATH)
+    inferencer.populate_df_general_prompts()
+    if is_gpt:
+        await inferencer.get_all_gpt35_responses()
+    else:
+        if torch.cuda.is_available():
+            model = AutoModelForCausalLM.from_pretrained(
+                MODEL_ID, torch_dtype=torch.float16, device_map="auto", cache_dir=CUSTOM_CACHE_DIR
+            )
+            tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, cache_dir=CUSTOM_CACHE_DIR)
+        else:
+            logger.error("CUDA is not available. This script requires a GPU to run.")
+            exit(1)
+        inferencer.get_all_llama_responses(model, tokenizer)
 
 
 if __name__ == "__main__":
-    OUTPUT_DIR.mkdir(exist_ok=True)
-    if torch.cuda.is_available():
-        model = AutoModelForCausalLM.from_pretrained(
-            MODEL_ID, torch_dtype=torch.float16, device_map="auto", cache_dir=CUSTOM_CACHE_DIR
-        )
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, cache_dir=CUSTOM_CACHE_DIR)
-    else:
-        logger.error("CUDA is not available. This script requires a GPU to run.")
-        exit(1)
-    inferencer = Inferencer(data_path=INPUT_FILEPATH)
-    inferencer.populate_df_general_prompts()
-    inferencer.get_all_llama_responses()
+    # client = OpenAI(api_key=get_openai_api_key())
+    # prompt = "What is 1+1?"
+    # try:
+    #     response = client.chat.completions.create(
+    #         model="gpt-3.5-turbo-0125", messages=[{"role": "user", "content": prompt}]
+    #     )
+    #     print(response.choices[0].message.content)
+    # except Exception as e:
+    #     print(f"Error processing prompt: {prompt}\nError: {e}")
+    #     print("Error in response")  # Placeholder or handle appropriately
+
+    asyncio.run(main())
